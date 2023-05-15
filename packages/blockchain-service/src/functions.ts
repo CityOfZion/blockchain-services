6012ce88ebc3d384a247e4077b0ee681a408a165
LAPTOP-HBL4M4SL\rauld <raulduartepe@gmail.com.br>
2023-05-15 18:26:33 -0300

    CU-864enn4ae - Fixes hasNNS and IsClaimable functions

--- packages/blockchain-service/src/functions.ts
+++ packages/blockchain-service/src/functions.ts
@@ -1,9 +1,9 @@
 import { BlockchainService, Claimable, NeoNameService } from "./interfaces"
 
 export function hasNNS(service: BlockchainService): service is NeoNameService & BlockchainService {
-  return "getNeoNsRecord" in service  && "getOwnerOfNeoNsRecord" in service && "validateNNSDomain" in service
+  return "getNNSRecord" in service  && "getOwnerOfNNS" in service && "validateNNSFormat" in service
 }
 
 export function isClaimable(service: BlockchainService): service is Claimable & BlockchainService {
-  return "claim" in this && "tokenClaim" in this && "getUnclaimed" in this.dataService
+  return "claim" in service && "tokenClaim" in service && "getUnclaimed" in service.dataService
 }
