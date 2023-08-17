09bfb74f80a7e284bbc106277c04b0fe17568570
Kenedy Ribeiro <helplace2019@gmail.com>
2023-08-17 14:37:49 -0300

    CU-86a0cw8d9 - Add support to CryptoCompare exchange

--- packages/bs-neo-legacy/src/__tests__/CryptoCompareExchange.spec.ts
+++ packages/bs-neo-legacy/src/__tests__/CryptoCompareExchange.spec.ts
@@ -4,25 +4,22 @@ import { BSNeoLegacy } from '../BSNeoLegacy'
 let bsNeoLegacy: BSNeoLegacy
 
 describe('CryptoCompare', () => {
-  beforeEach(() => {
-    bsNeoLegacy = new BSNeoLegacy('neoLegacy', { type: 'testnet', url: 'http://seed5.ngd.network:20332' })
+  beforeAll(() => {
+    bsNeoLegacy = new BSNeoLegacy('neoLegacy', { type: 'mainnet', url: '"https://mainnet1.neo2.coz.io:443' })
   })
   it('Should return a list with prices of tokens using USD', async () => {
-    bsNeoLegacy.network.type = 'mainnet'
     const currency: Currency = 'USD'
     const tokenPriceList = await bsNeoLegacy.exchange.getTokenPrices(currency)
     expect(tokenPriceList.length).toBeGreaterThan(0)
   })
 
   it('Should return a list with prices of tokens using BRL', async () => {
-    bsNeoLegacy.network.type = 'mainnet'
     const currency: Currency = 'BRL'
     const tokenPriceList = await bsNeoLegacy.exchange.getTokenPrices(currency)
     expect(tokenPriceList.length).toBeGreaterThan(0)
   })
 
   it('Should return a list with prices of tokens using EUR', async () => {
-    bsNeoLegacy.network.type = 'mainnet'
     const currency: Currency = 'EUR'
     const tokenPriceList = await bsNeoLegacy.exchange.getTokenPrices(currency)
     expect(tokenPriceList.length).toBeGreaterThan(0)
