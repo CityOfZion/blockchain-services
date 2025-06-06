44b81b508bc8a794ad68d6a337fdbc0da9bde254
hotequil <hotequil@protonmail.com>
2025-06-06 10:10:42 -0300

    CU-86a98a40k-BS Lib - Swap - Create unit test for calculate fee between ETH and NEO (N3)

--- packages/bs-swap/src/env.d.ts
+++ packages/bs-swap/src/env.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       TEST_PRIVATE_KEY: string
       TEST_PRIVATE_KEY_TO_SWAP_TOKEN: string
+      TEST_ETHEREUM_PRIVATE_KEY: string
       TEST_ADDRESS_TO_SWAP_TOKEN: string
       TEST_SWAP_ID: string
       TEST_XRP_ADDRESS_TO_SWAP_TOKEN: string
