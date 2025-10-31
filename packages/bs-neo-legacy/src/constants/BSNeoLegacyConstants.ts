f90634f9be6a00403953faa945647054607414ae
hotequil <hotequil@protonmail.com>
2025-10-31 17:19:06 -0300

    CU-86acyrw6u-BS Lib - Remove Neo Legacy migration (cleanup)

--- packages/bs-neo-legacy/src/constants/BSNeoLegacyConstants.ts
+++ packages/bs-neo-legacy/src/constants/BSNeoLegacyConstants.ts
@@ -18,22 +18,14 @@ export class BSNeoLegacyConstants {
 
   static readonly RPC_LIST_BY_NETWORK_ID: Record<TBSNeoLegacyNetworkId, string[]> = {
     mainnet: [
-      'https://mainnet1.neo2.coz.io:443',
-      'https://mainnet2.neo2.coz.io:443',
-      'https://mainnet3.neo2.coz.io:443',
-      'http://seed9.ngd.network:10332',
       'http://seed1.ngd.network:10332',
       'http://seed2.ngd.network:10332',
       'http://seed3.ngd.network:10332',
       'http://seed4.ngd.network:10332',
       'http://seed5.ngd.network:10332',
+      'http://seed9.ngd.network:10332',
     ],
-    testnet: [
-      'http://seed5.ngd.network:20332',
-      'http://seed1.ngd.network:20332',
-      'http://seed2.ngd.network:20332',
-      'https://testnet1.neo2.coz.io:443',
-    ],
+    testnet: ['http://seed1.ngd.network:20332', 'http://seed2.ngd.network:20332', 'http://seed5.ngd.network:20332'],
   }
 
   static readonly LEGACY_NETWORK_BY_NETWORK_ID: Record<TBSNeoLegacyNetworkId, string> = {
@@ -56,13 +48,6 @@ export class BSNeoLegacyConstants {
   static readonly ALL_NETWORKS: TBSNetwork<TBSNeoLegacyNetworkId>[] = [this.MAINNET_NETWORK, this.TESTNET_NETWORK]
 
   static readonly DEFAULT_BIP44_DERIVATION_PATH = "m/44'/888'/0'/0/?"
-
-  static readonly MIGRATION_COZ_LEGACY_ADDRESS = 'AaT27thuyPaqERPwERhk7QhfKrbj4xoyAV'
-  static readonly MIGRATION_COZ_FEE = 0.01 // 1%
-  static readonly MIGRATION_NEP_17_TRANSFER_FEE = 0.0112143
-  static readonly MIGRATION_MIN_GAS = 0.1
-  static readonly MIGRATION_MIN_NEO = 2
-  static readonly MIGRATION_COZ_NEO3_ADDRESS = 'NLMsicDapULKFDmAzTsbhwrZjYZ83j53Ty'
 
   static readonly MAX_TRANSACTION_SIZE_WITHOUT_FEE = 1024
   static readonly FEE_APPLIED_TO_PLAYABLE_TRANSACTION = 0.05
