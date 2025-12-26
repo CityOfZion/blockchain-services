8afe3b9d55e30d6d150af853a741d9777359ef3e
hotequil <hotequil@protonmail.com>
2025-12-26 18:02:39 -0300

    CU-86acqgcny-BS Lib - Research and implement the Anti-MEV (Neo X)

--- packages/bs-neox/jest.setup.ts
+++ packages/bs-neox/jest.setup.ts
@@ -1,10 +1,1 @@
 import 'dotenv/config'
-
-// Necessary to run on Node.js
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-if (typeof self === 'undefined') {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  global.self = global
-}
