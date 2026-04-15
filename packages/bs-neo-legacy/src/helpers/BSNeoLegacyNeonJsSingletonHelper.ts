7ee7ff14ecb78d797b7d814eb34385424f1e7cba
raulduartep <raulduartepe@gmail.com>
2026-04-15 14:30:24 -0300

    CU-86agx4494 - BS Lib  - Remove BSNeoLegacy neon-js library to avoid conflicts with BSNeo3 neon-js library

--- packages/bs-neo-legacy/src/helpers/BSNeoLegacyNeonJsSingletonHelper.ts
+++ packages/bs-neo-legacy/src/helpers/BSNeoLegacyNeonJsSingletonHelper.ts
@@ -1,15 +1,19 @@
-import type * as NeonJs from '@cityofzion/neon-js'
-
+import type * as NeonJsCore from '@cityofzion/neon-core-legacy'
+import type * as NeonJsApi from '@cityofzion/neon-api-legacy'
 export class BSNeoLegacyNeonJsSingletonHelper {
-  static #instance: typeof NeonJs
+  static #instanceCore: typeof NeonJsCore
+  static #instanceApi: typeof NeonJsApi
 
   static getInstance() {
-    if (!BSNeoLegacyNeonJsSingletonHelper.#instance) {
-      BSNeoLegacyNeonJsSingletonHelper.#instance = require('@cityofzion/neon-js')
+    if (!BSNeoLegacyNeonJsSingletonHelper.#instanceCore) {
+      BSNeoLegacyNeonJsSingletonHelper.#instanceCore = require('@cityofzion/neon-core-legacy')
+    }
+    if (!BSNeoLegacyNeonJsSingletonHelper.#instanceApi) {
+      BSNeoLegacyNeonJsSingletonHelper.#instanceApi = require('@cityofzion/neon-api-legacy')
     }
 
-    return BSNeoLegacyNeonJsSingletonHelper.#instance
+    return { ...BSNeoLegacyNeonJsSingletonHelper.#instanceCore, api: BSNeoLegacyNeonJsSingletonHelper.#instanceApi }
   }
 }
 
-export type * from '@cityofzion/neon-js'
+export type * from '@cityofzion/neon-core-legacy'
